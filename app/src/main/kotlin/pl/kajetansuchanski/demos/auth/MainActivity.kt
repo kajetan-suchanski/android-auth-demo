8a6791821305f901aced970d2d409417be966380
kaj <KajetanSuchanski@gmail.com>
2021-01-02 19:44:28 +0100

    Secured device required message

--- app/src/main/kotlin/pl/kajetansuchanski/demos/auth/MainActivity.kt
+++ app/src/main/kotlin/pl/kajetansuchanski/demos/auth/MainActivity.kt
@@ -1,7 +1,11 @@
 package pl.kajetansuchanski.demos.auth
 
+import android.app.KeyguardManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,13 +13,31 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val fragment = Fragment()
-        supportFragmentManager.beginTransaction().add(fragment, null).commit()
-
         button_authenticate.setOnClickListener {
-            BiometricAuth.prompt(fragment) { status, result ->
+            tryAuthenticate()
+        }
+    }
 
+    private fun tryAuthenticate() {
+        val keyguardManager = getSystemService<KeyguardManager>()!!
+        val isSecure = with(keyguardManager) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                isDeviceSecure
+            } else {
+                isKeyguardSecure
             }
+        }
+
+        if (!isSecure) {
+            Toast.makeText(this, R.string.secured_device_required, Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val fragment = Fragment()
+        supportFragmentManager.beginTransaction().add(fragment, null).commitNow()
+
+        BiometricAuth.prompt(fragment) { status, result ->
+
         }
     }
 }
